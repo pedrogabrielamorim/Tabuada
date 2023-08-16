d1218c9d98ad28ce662b857664d66f94cf716bfd
Pedro Gabriel Amorim <pedroamorim7.pga@yahoo.com>
2023-08-16 17:08:54 -0300

    First commit

--- src/Calc/Tabuada.java
+++ src/Calc/Tabuada.java
@@ -1,2 +1,16 @@
-package Calc;public class Tabuada {
+package Calc;
+
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        int n = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe a tabuada que deseja: ");
+        n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++)
+            System.out.println(n + "x" + i + "= " + n * i);
+    }
 }
